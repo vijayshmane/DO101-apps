b4f505a11b39dcf935672861addc8e7df128dbfe
vijayshmane <46953923+vijayshmane@users.noreply.github.com>
2020-10-19 22:23:17 -0400

    Update app.js

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is 1 version of the app.' + '\n';
+    response = 'This is 2 version of the app.' + '\n';
 
     //send the response to the client
     res.send(response);
