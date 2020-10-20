f4e011742c89963e9ed900eb5b1ca6fcbadca562
vijayshmane <46953923+vijayshmane@users.noreply.github.com>
2020-10-20 16:20:35 -0400

    Update app.js

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is 1 version of the app.' + '\n';
+    response = 'This is webhook deploy version of the app.' + '\n';
 
     //send the response to the client
     res.send(response);
